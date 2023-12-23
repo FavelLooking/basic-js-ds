ac0ed985c38ba32b95d8dcbd67a7492a0657d11c
FavelLooking <intwestpol@gmail.com>
2023-12-24 00:17:51 +0400

    feat: solve remove from list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+
+  let newNode = new ListNode(0);
+  newNode.next = l;
+
+  let prev = newNode; 
+  let current = l;
+
+  while (current !== null) {
+    if (current.value === k) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+
+  return newNode.next;
 }
 
 module.exports = {
